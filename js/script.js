f242ff913ab2124e2acb90cb13060567add93264
valyafrankova22 <vfrankova22@icloud.com>
2021-10-07 14:48:43 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 // Task #1
+const example1 = Boolean( Number(`10`)) + 1; // 2, number
+const example2 = `sub` + Number(false); // sub0, string
+const example3 = 16 * ` 91 `; // 1456, number
+const example4 = true-70; // -69, number
+const example5 = Number(1 + String(1)) + 1; // 12, number
+
+console.log(typeof example1);
+console.log(typeof example2);
+console.log(typeof example3);
+console.log(typeof example4);
+console.log(typeof example5);
+
 
 
 
